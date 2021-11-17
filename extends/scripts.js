bf8557dad8851a81d5aca6af3cc5eddc7dea85d8
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2021-11-16 23:07:54 -0300

    Edit Chart.js

--- extends/scripts.js
+++ extends/scripts.js
@@ -13,13 +13,6 @@ var chartGraph = new Chart(ctx, {
             borderWidth: 5,
             borderColor: 'rgba(77,166,254,0.85)',
             
-        },
-        {
-            label: "Volume medio útil de cada Hidrelétrica em %", 
-            data: [15.55, 17.02, 10.12, 9.74, 37.26, 34.40, 38.02, 22.57],
-            borderWidth: 5,
-            borderColor: 'rgba(77,166,254,0.85)',
-            
         }]
     },
 });
