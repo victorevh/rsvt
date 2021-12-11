97223a4678a356feb1b986002cd3dafadf2dbd2d
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2021-12-11 12:51:35 -0300

    RSVT: Adiciona scripts de interação

---
+++ extends/interateScript.js
@@ -0,0 +1,54 @@
+// Menu
+class MobileNavbar {
+    
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+
+mobileNavbar.init()
+
+// Seções
+
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "block")
+        document.getElementById(el).style.display = 'none';
+    else
+        document.getElementById(el).style.display = 'block';
+}
